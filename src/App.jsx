84cd7735dcb31dcf65e2cce32220287666e4e937
FahimTasnimKhan <fahimtasnimkhan@gmail.com>
2026-05-31 20:57:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+    </>
+  );
+}
+
+export default App;
